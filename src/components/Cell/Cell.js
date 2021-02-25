24367229af35d2cc8cac58ff0055f7b25d66ca60
slavaider <slavaider1@gmail.com>
2021-02-25 18:32:45 +0500

    init: start task

---
+++ src/components/Cell/Cell.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import './Cell.css'
+import Cross from '../../assets/img/cross.png'
+import Null from '../../assets/img/null.png'
+
+function Cell(props) {
+    const style = {backgroundImage: null}
+    if (props.value === 1) {
+        style.backgroundImage = `url(${Cross})`
+    }
+    if (props.value === 2) {
+        style.backgroundImage = `url(${Null})`
+    }
+    return (
+        <td>
+            <input
+                type={'button'}
+                style={style}
+                disabled={props.value !== 0}
+                onClick={props.onClickHandler.bind(this, props.index)}
+            >
+            </input>
+        </td>
+    )
+}
+
+export default Cell
